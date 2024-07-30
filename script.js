1f74b696cb0fb35e9159020199d0dc7a8a14feef
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-07-30 21:20:01 +0400

    add some styles : improve visual effects

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-"use strict";
-
 document.addEventListener("DOMContentLoaded", () => {
   const headers = document.querySelectorAll(".accordion-header");
   headers.forEach((header) => {
@@ -7,14 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
       const content = header.nextElementSibling;
       const isOpen = content.style.maxHeight;
 
+      // Close all open accordion items
       document.querySelectorAll(".accordion-content").forEach((item) => {
         item.style.maxHeight = null;
-        item.style.padding = "0 15px";
+        item.style.padding = "0 20px";
       });
 
+      // Open the clicked accordion item if it was closed
       if (!isOpen) {
         content.style.maxHeight = content.scrollHeight + "px";
-        content.style.padding = "15px";
+        content.style.padding = "20px";
       }
     });
   });
