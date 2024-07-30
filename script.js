c6d8b525b728b832e162644a63f51e9865bd5b01
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-07-30 21:15:04 +0400

    add transition effects

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+"use strict";
+
 document.addEventListener("DOMContentLoaded", () => {
   const headers = document.querySelectorAll(".accordion-header");
   headers.forEach((header) => {
     header.addEventListener("click", () => {
       const content = header.nextElementSibling;
-      const isOpen = content.style.display === "block";
-      document
-        .querySelectorAll(".accordion-content")
-        .forEach((item) => (item.style.display = "none"));
+      const isOpen = content.style.maxHeight;
+
+      // Close all open accordion items
+      document.querySelectorAll(".accordion-content").forEach((item) => {
+        item.style.maxHeight = null;
+        item.style.padding = "0 15px";
+      });
+
+      // Open the clicked accordion item if it was closed
       if (!isOpen) {
-        content.style.display = "block";
+        content.style.maxHeight = content.scrollHeight + "px";
+        content.style.padding = "15px";
       }
     });
   });
