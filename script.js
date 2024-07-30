df0fffb6d87510e04e5a80e497e8eb387611cbb5
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-07-30 21:17:06 +0400

    add paragraphs

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ document.addEventListener("DOMContentLoaded", () => {
       const content = header.nextElementSibling;
       const isOpen = content.style.maxHeight;
 
-      // Close all open accordion items
       document.querySelectorAll(".accordion-content").forEach((item) => {
         item.style.maxHeight = null;
         item.style.padding = "0 15px";
       });
 
-      // Open the clicked accordion item if it was closed
       if (!isOpen) {
         content.style.maxHeight = content.scrollHeight + "px";
         content.style.padding = "15px";
